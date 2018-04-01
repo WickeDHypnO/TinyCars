50e8e06dc3dd1be91fc3be87bcd4052636c4e0ee
Krystian Lison <krystianlison@wenklystudio.com>
2018-04-01 20:50:22 +0200

    Testowy model itp.

---
+++ Assets/CarModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheels
+{
+    public GameObject frontLeftWheel;
+    public GameObject frontRightWheel;
+    public GameObject backLeftWheel;
+    public GameObject backRightWheel;
+}
+
+
+public class CarModel : MonoBehaviour {
+
+    public MeshFilter model;
+    public MeshRenderer renderer;
+    public Wheels wheels;
+
+	void Start () {
+        var carProps = FindObjectOfType<GameManager>().car;
+        model.mesh = carProps.model;
+        renderer.material = carProps.material;
+        model.transform.localScale = new Vector3(carProps.scale, carProps.scale, carProps.scale);
+        model.GetComponentInParent<BoxCollider>().size = carProps.colliderBounds;
+        model.transform.localPosition = carProps.offset;
+        wheels.frontRightWheel.transform.localPosition = carProps.frontWheelOffset;
+        wheels.frontLeftWheel.transform.localPosition = new Vector3(-carProps.frontWheelOffset.x, carProps.frontWheelOffset.y, carProps.frontWheelOffset.z);
+        wheels.backRightWheel.transform.localPosition = carProps.backWheelOffset;
+        wheels.backLeftWheel.transform.localPosition = new Vector3(-carProps.backWheelOffset.x, carProps.backWheelOffset.y, carProps.backWheelOffset.z);
+        wheels.frontLeftWheel.GetComponent<MeshFilter>().mesh = carProps.wheelModel;
+        wheels.frontRightWheel.GetComponent<MeshFilter>().mesh = carProps.wheelModel;
+        wheels.backLeftWheel.GetComponent<MeshFilter>().mesh = carProps.wheelModel;
+        wheels.backRightWheel.GetComponent<MeshFilter>().mesh = carProps.wheelModel;
+        wheels.frontLeftWheel.transform.localScale = carProps.wheelScale;
+        wheels.frontRightWheel.transform.localScale = carProps.wheelScale;
+        wheels.backLeftWheel.transform.localScale = carProps.wheelScale;
+        wheels.backRightWheel.transform.localScale = carProps.wheelScale;
+    }
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/CarProperties.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+#if UNITY_EDITOR
+using System.IO;
+using UnityEngine;
+#endif
+
+public class CarProperties : ScriptableObject {
+
+    public Mesh model;
+    public Material material;
+    public Vector3 offset;
+    public Vector3 colliderBounds;
+    public float scale;
+    public Mesh wheelModel;
+    public Vector3 frontWheelOffset;
+    public Vector3 backWheelOffset;
+    public Vector3 wheelScale;
+
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/CarProperties")]
+    public static void CreatCar()
+    {
+        var asset = CreateInstance<CarProperties>();
+        AssetDatabase.CreateAsset(asset, "Assets/CarProperties/New Car.asset");
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+#endif
+}
